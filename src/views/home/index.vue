a3452d639968b4e9db2c69b9c3ac12afadb0f3e6
Han-0826 <64177692+Han-0826@users.noreply.github.com>
2022-01-24 11:26:59 +0800

    Add files via upload

---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home"></div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+#home {
+  width: 100%;
+  height: 100%;
+}
+</style>
